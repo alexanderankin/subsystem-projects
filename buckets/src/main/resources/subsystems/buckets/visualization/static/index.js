8d161414b328c2290a897bb471c90ca6b5cd32f9
Dave Ankin <daveankin@gmail.com>
2023-01-15 15:20:30 -0500

    jsdoc types, constants, logging, rendering, non-minified d3

--- buckets/src/main/resources/subsystems/buckets/visualization/static/index.js
+++ buckets/src/main/resources/subsystems/buckets/visualization/static/index.js
@@ -1,3 +1,34 @@
+let levels = {
+  error: 200,
+  warn: 300,
+  info: 400,
+  debug: 500,
+  trace: 600,
+}
+let level = levels['info'];
+
+function noop() {
+}
+
+let logger = Object.entries(levels)
+  .reduce((l, [n, num]) => { l[n] = level > num ? console.log : noop; return l; }, {});
+
+// let logger = {
+//   debug: level >= levels.debug ? console.log : noop,
+//   etc ...
+// }
+
+const RING_DIFFERENCE = 30;
+const RING_BASE = 100;
+
+/**
+ * @typedef {
+ *  name: string,
+ *  color: string,
+ *  location: 'center'|'inner'|'middle'|'outer'|false,
+ *  orientation: 'left'|'center'|'right'|false
+ * } Bucket
+ */
 $(() => {
   let scenarioGraphicElement = document.querySelector('svg#scenario-graphic');
   adjustSvgSize();
@@ -5,8 +36,94 @@ $(() => {
 
   let scenarioGraphic = d3.select('svg#scenario-graphic');
 
-  setupConcentricCircles(scenarioGraphicElement, scenarioGraphic);
+  let center = setupConcentricCircles(scenarioGraphicElement, scenarioGraphic);
+
+  /**
+   * @type {Bucket[]}
+   */
+  let bodies = [
+    {
+      name: 'observer',
+      color: 'green',
+      strokeWidth: 3,
+      location: 'inner',
+      orientation: 'left'
+    },
+    {
+      name: 'observed',
+      color: 'red',
+      location: 'middle',
+      orientation: 'right',
+    },
+    {
+      name: 'center',
+      color: 'black',
+    },
+  ];
+
+  renderBodies(scenarioGraphic, center, bodies);
+
+  window.Bodies = { bodies, render: renderBodies };
 });
+
+/**
+ * @param {d3.select} scenarioGraphic
+ * @param {{x: number, y: number}} center
+ * @param {Bucket[]} bodies
+ * @return {Array} newly rendered things
+ */
+function renderBodies(scenarioGraphic, center, bodies) {
+  for (let i = 0; i < bodies.length; i++) {
+    let body = bodies[i];
+    let {
+      name,
+      color,
+      location,
+      orientation
+    } = body;
+
+    if (!location || !orientation) {
+      logger.debug('not appending', name, 'didn\'t have location/orientation', body)
+      continue;
+    }
+
+    let { x, y } = center;
+
+    if (name !== 'center') {
+      let right = orientation === 'right';
+      x += RING_BASE * (right ? 1 : -1);
+
+      x += RING_DIFFERENCE * ({
+        inner: right ? -1 : 1,
+        middle: 0,
+        outer: right ? 1 : -1,
+      }[location])
+
+      // should make some constants
+      let yDiff = 16 + ({
+        inner: -6,
+        middle: 0,
+        outer: 6,
+      }[location])
+      logger.trace(yDiff, body)
+      y += right ? -yDiff : yDiff;
+
+      logger.debug('name', name, 'using plotted case (not center)', { offsets: { x, y } })
+    }
+
+    let newCircle = scenarioGraphic.append('circle')
+      .attr('cx', x)
+      .attr('cy', y)
+      .attr('r', 20)
+      .style('fill', '#0000')
+      .style('stroke', color || '#000')
+      .style('stroke-width', body.strokeWidth || 2)
+
+    // replace
+    if (body.circle && typeof body.circle.remove === 'function') body.circle.remove();
+    body.circle = newCircle;
+  }
+}
 
 function adjustSvgSize() {
   const scenarioGraphicElement = document.querySelector('svg#scenario-graphic');
@@ -20,6 +137,11 @@ function adjustSvgSize() {
   scenarioGraphicElement.style.width = pWidth + 'px';
 }
 
+/**
+ * @param {Element} scenarioGraphicElement
+ * @param {*} scenarioGraphic
+ * @return {{x:Number, y:Number}}
+ */
 function setupConcentricCircles(scenarioGraphicElement, scenarioGraphic) {
   let {
     clientHeight: height,
@@ -27,7 +149,7 @@ function setupConcentricCircles(scenarioGraphicElement, scenarioGraphic) {
   } = scenarioGraphicElement;
 
 
-  for (let radius of [-1, 0, 1].map(e => 100 + (30 * e))) {
+  for (let radius of [-1, 0, 1].map(e => RING_BASE + (RING_DIFFERENCE * e))) {
     // http://www.d3noob.org/2014/02/styles-in-d3js.html
     scenarioGraphic.append('circle')
       .attr('cx', ~~(width / 2))
@@ -37,5 +159,10 @@ function setupConcentricCircles(scenarioGraphicElement, scenarioGraphic) {
       .style('stroke', '#000')
       .style('stroke-width', 1)
     ;
+  }
+
+  return {
+    x: ~~(width / 2),
+    y: ~~(height / 2),
   }
 }
